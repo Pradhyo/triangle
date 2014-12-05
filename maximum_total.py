9e623529606a102fe32bab1f591930f3754e862a
Pradhyo Bijja <pradhyo@nyu.edu>
2014-12-05 17:03:24 -0500

    Add loop to find maximum total

--- maximum_total.py
+++ maximum_total.py
@@ -1,8 +1,15 @@
-
+total = 0
+offset = 0
 
 with open('sample.txt') as fp:
 	for line in fp:
 		x = line.split()
-		print x
+		if len(x) > 1:
+			if int(x[offset]) < int(x[offset+1]):
+				offset = offset + 1
+			total = total + int(x[offset])
+		else:
+			total = total + int(x[offset])
+print total
 
 	
