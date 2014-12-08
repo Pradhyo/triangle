747ca7dd1c4184a3ed33053da214c7547fa2c2de
Pradhyo Bijja <pradhyo@nyu.edu>
2014-12-07 23:31:51 -0500

    Store everything as integers in 2D list

--- maximum_total.py
+++ maximum_total.py
@@ -1,13 +1,9 @@
 total = 0
 offset = 0
+values = []
 
 with open('sample.txt') as fp:
 	for line in fp:
 		x = line.split()
-		if len(x) > 1:
-			if int(x[offset]) < int(x[offset+1]):
-				offset = offset + 1
-		total = total + int(x[offset])
-print total
-
-	
+		x = map (int,x)
+		values.append(x)
