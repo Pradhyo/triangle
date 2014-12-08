d8820bcbb8e22bd1f48b18574fd8148c25e8cd3c
Pradhyo Bijja <pradhyo@nyu.edu>
2014-12-08 00:48:16 -0500

    Add recursive function to compare totals

--- maximum_total.py
+++ maximum_total.py
@@ -2,8 +2,18 @@ total = 0
 offset = 0
 values = []
 
+def max(row,column):
+	if row == len(values)-1:
+		return values[row][column]
+	elif max(row+1,column) > max(row+1,column+1):
+		return values[row][column]+max(row+1,column)
+	else:
+		return values[row][column] + max(row+1,column+1)
+
 with open('sample.txt') as fp:
 	for line in fp:
 		x = line.split()
 		x = map (int,x)
 		values.append(x)
+
+print max(0,0)
