0649df7178cb45c4f4dba502341196ec7d2a3311
Pradhyo Bijja <pradhyo@nyu.edu>
2015-02-07 16:14:11 -0500

    Store each calculated value in 2d dictionary

--- maximum_total.py
+++ maximum_total.py
@@ -1,15 +1,21 @@
+from collections import defaultdict
 total = 0
 offset = 0
 values = []
+mem_max = defaultdict(lambda:defaultdict(list))
 
 def max(row,column):
-	if row == len(values)-1:
-		x = values[row][column]
-	elif max(row+1,column) > max(row+1,column+1):
-		x = values[row][column]+max(row+1,column)
+	if mem_max[row][column]:
+		return mem_max[row][column]
 	else:
-		x = values[row][column] + max(row+1,column+1)
-	return x
+		if row == len(values)-1:
+			x = values[row][column]
+		elif max(row+1,column) > max(row+1,column+1):
+			x = values[row][column]+max(row+1,column)
+		else:
+			x = values[row][column] + max(row+1,column+1)
+		mem_max[row][column] = x
+		return x
 
 with open('triangle.txt') as fp:
 	for line in fp:
