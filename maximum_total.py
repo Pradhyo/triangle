d11882b9acee3e9741b8948e3e70c145c0cf5bf6
Pradhyo Bijja <pradhyo@nyu.edu>
2015-02-07 23:00:34 -0500

    Rename max function as max_total

--- maximum_total.py
+++ maximum_total.py
@@ -4,23 +4,23 @@ offset = 0
 values = []
 mem_max = defaultdict(lambda:defaultdict(list))
 
-def max(row,column):
+def max_total(row,column):
 	if mem_max[row][column]:
 		return mem_max[row][column]
 	else:
 		if row == len(values)-1:
 			x = values[row][column]
-		elif max(row+1,column) > max(row+1,column+1):
-			x = values[row][column]+max(row+1,column)
+		elif max_total(row+1,column) > max_total(row+1,column+1):
+			x = values[row][column] + max_total(row+1,column)
 		else:
-			x = values[row][column] + max(row+1,column+1)
+			x = values[row][column] + max_total(row+1,column+1)
 		mem_max[row][column] = x
 		return x
 
 with open('triangle.txt') as fp:
 	for line in fp:
 		x = line.split()
-		x = map (int,x)
+		x = map(int,x)
 		values.append(x)
 
-print max(0,0)
+print max_total(0,0)
