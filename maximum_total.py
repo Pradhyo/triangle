5709abc8618b40f183298dda9981e91f78bca514
Pradhyo Bijja <pradhyo@nyu.edu>
2015-02-07 23:41:25 -0500

    Replace recursive function with for loop

--- maximum_total.py
+++ maximum_total.py
@@ -4,22 +4,18 @@ offset = 0
 values = []
 mem_max = defaultdict(lambda:defaultdict(list))
 
-def max_total(row,column):
-	if mem_max[row][column]:
-		return mem_max[row][column]
-	else:
-		if row == len(values)-1:
-			mem_max[row][column] = values[row][column]
-		else:
-			mem_max[row][column] = values[row][column] + max(max_total(row+1,column),max_total(row+1,column+1))
-		
-		return mem_max[row][column]
-		
-
 with open('triangle.txt') as fp:
 	for line in fp:
 		x = line.split()
 		x = map(int,x)
 		values.append(x)
 
-print max_total(0,0)
+for row in range(len(values)-1,-1,-1):
+	for column in range(len(values[row])):
+		if row == len(values)-1:
+			mem_max[row][column] = values[row][column]
+		else:
+			mem_max[row][column] = values[row][column] + max(mem_max[row+1][column],mem_max[row+1][column+1])
+
+
+print mem_max[0][0]
