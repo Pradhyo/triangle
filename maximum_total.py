6261bd35b9ea10d171df4d9ff627d31b5a560965
Pradhyo Bijja <pradhyo@nyu.edu>
2015-02-08 00:00:37 -0500

    Add comments

--- maximum_total.py
+++ maximum_total.py
@@ -1,15 +1,16 @@
 from collections import defaultdict
-total = 0
-offset = 0
 values = []
 mem_max = defaultdict(lambda:defaultdict(list))
 
+'''Store given values from file in a list of lists'''
 with open('triangle.txt') as fp:
 	for line in fp:
 		x = line.split()
 		x = map(int,x)
 		values.append(x)
 
+'''Loop from bottom to top, looping through each element of a row
+   solving each sub-problem until the top'''
 for row in range(len(values)-1,-1,-1):
 	for column in range(len(values[row])):
 		mem_max[row][column] = values[row][column]	
